01d92b1949cfd2d2d51c1e473f560a48ce070a62
Pranay-Blocsys <pranay@blocsys.com>
2024-04-17 16:09:03 +0530

    feat: base commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import "./App.css";
+import Wallet from "./component/wallet/wallet";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Wallet />
+      </header>
+    </div>
+  );
+}
+
+export default App;
